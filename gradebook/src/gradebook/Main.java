25776a94b08fe238dfb7499bc696890c689e8561
Sasa Joksovic <36681876+SasaOrca@users.noreply.github.com>
2018-02-27 10:55:38 +0100

    Gradebook

--- gradebook/src/gradebook/Main.java
+++ gradebook/src/gradebook/Main.java
@@ -15,55 +15,55 @@ public class Main{
 	    String student5;
 	    
 	    
-		double ocenaStudenta1;
-		double ocenaStudenta2;
-		double ocenaStudenta3;
-		double ocenaStudenta4;
-		double ocenaStudenta5;
+		double gradeStudent1;
+		double gradeStudent2;
+		double gradeStudent3;
+		double gradeStudent4;
+		double gradeStudent5;
 		
 		
-		System.out.println("Upisi ime i prezime prvog studenta:");
+		System.out.println("Enter the first and last name of the first student:");
 		String Studenta1 = sc.next();
-		System.out.println("Upisi ocenu prvog studenta:");
-		ocenaStudenta1 = new Scanner(System.in).nextDouble();
+		System.out.println("Write the grade of the first student:");
+		gradeStudent1 = new Scanner(System.in).nextDouble();
 		
-		System.out.println("Upisi ime i prezime druog studenta:");
+		System.out.println("Enter the first and last name of the second student:");
 		String Studenta2 = sc.next();
-		System.out.println("Upisi ocenu drugog sudenta:");
-		ocenaStudenta2 = new Scanner(System.in).nextDouble();
+		System.out.println("Write the grade of the second student:");
+		gradeStudent2 = new Scanner(System.in).nextDouble();
 		
-		System.out.println("Upisi ime i prezime treceg studenta:");
+		System.out.println("Enter the first and last name of the third student:");
 		String Studenta3 = sc.next();
-		System.out.println("Upisi ocenu treceg studenta:");
-		ocenaStudenta3 = new Scanner(System.in).nextDouble();
+		System.out.println("Write the grade of the third student:");
+		gradeStudent3 = new Scanner(System.in).nextDouble();
 		
-		System.out.println("Upisi ime i prezime cetvrtog studenta:");
+		System.out.println("Enter the first and last name of the fourth student:");
 		String Studenta4 = sc.next();
-		System.out.println("Upisi ocenu cetvrtog sudenta:");
-		ocenaStudenta4 = new Scanner(System.in).nextDouble();
+		System.out.println("Write the grade of the fourth student:");
+		gradeStudent4 = new Scanner(System.in).nextDouble();
 		
-		System.out.println("Upisi ime i prezime petog studenta:");
+		System.out.println("Enter the first and last name of the fifth student:");
 		String Studenta5 = sc.next();
-		System.out.println("Upisi ocenu petog studenta:");
-		ocenaStudenta5 = new Scanner(System.in).nextDouble();
+		System.out.println("Write the grade of the fifth student:");
+		gradeStudent5 = new Scanner(System.in).nextDouble();
 		
 		
 		
-		double odgovor = ocenaStudenta1 + ocenaStudenta2 + ocenaStudenta3 + ocenaStudenta4 + ocenaStudenta5;
+		double SumOfAllGrades = gradeStudent1 + gradeStudent2 + gradeStudent3 + gradeStudent4 + gradeStudent5;
 		
 		
-		System.out.println("Razred Gospodje Dzejms:");
+		System.out.println("Class of the Mrs.James:");
 		
-		System.out.println("Student -" + "student1" + " njegova ocena je " + ocenaStudenta1);
-		System.out.println("Student -" + "student2" + " njegova ocena je " + ocenaStudenta2);
-		System.out.println("Student -" + "student3" + " njegova ocena je " + ocenaStudenta3);
-		System.out.println("Student -" + "student4" + " njegova ocena je " + ocenaStudenta4);
-		System.out.println("Student -" + "student5" + " njegova ocena je " + ocenaStudenta5);
-		
-		
+		System.out.println("Student -" + "student1" + " the grade is " + gradeStudent1);
+		System.out.println("Student -" + "student2" + " the grade is " + gradeStudent2);
+		System.out.println("Student -" + "student3" + " the grade is " + gradeStudent3);
+		System.out.println("Student -" + "student4" + " the grade is " + gradeStudent4);
+		System.out.println("Student -" + "student5" + " the grade is " + gradeStudent5);
 		
 		
-		System.out.println("Zajednicki zbir ocena studenata je:" +  odgovor);
+		
+		
+		System.out.println("The total sum of the grades is:" +  SumOfAllGrades);
 		
 	
 		
