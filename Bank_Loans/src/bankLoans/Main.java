19defd00b95f7644f6d0a35c141fe1b55b0375d8
Sasa Joksovic <36681876+SasaOrca@users.noreply.github.com>
2018-03-09 13:19:11 +0100

    Bank_Loans

---
+++ Bank_Loans/src/bankLoans/Main.java
@@ -0,0 +1,15 @@
+package bankLoans;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		try(Scanner sc = new Scanner(System.in)) {
+			
+			
+
+		}
+	}
+}
