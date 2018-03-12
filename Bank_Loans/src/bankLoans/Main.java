b06ee6a43024b680333f170214de09136dd2c3b6
Sasa Joksovic <36681876+SasaOrca@users.noreply.github.com>
2018-03-12 19:16:45 +0100

    update

--- Bank_Loans/src/bankLoans/Main.java
+++ Bank_Loans/src/bankLoans/Main.java
@@ -21,29 +21,29 @@ public class Main {
 			
 			if (loanType.equals("cash")) {
 			
-			System.out.println("What is your name?");
-			String clientName = sc.next();
-			System.out.println("Is your surname?");
-			String clientSurName = sc.next();
-			System.out.println("How old are you?");
-			int Age1 = sc.nextInt();
-			System.out.println("How old are you working years?");
-			int YearsOfService = sc.nextInt();
-			System.out.println("Amount of your salary?");
-			int Salary = sc.nextInt();
-			System.out.println("Are you employed? Answer with: true or false.");
-			boolean Employed = sc.nextBoolean();
-			
-			if(Age1 >= 18 && YearsOfService >= 1 && Salary >= 300 && Employed == true) {
-				System.out.println("Your cash loan is ACCEPTED");
-			}else 
-				System.out.println("Your cash loan is NOT ACCEPTED");
-			
-			System.out.println("==========================================================================================================");
-			
-			System.out.println("Your village's data for cash loan is ---Name: " + clientName + "; Surname: " + clientSurName + "; Years: "  + Age1 + "; Years of service: " + YearsOfService + "; Salary: " + Salary + "; State of employment: " + Employed);
-}
-}
-}
+				System.out.println("What is your name?");
+				String clientName = sc.next();
+				System.out.println("Is your surname?");
+				String clientSurName = sc.next();
+				System.out.println("How old are you?");
+				int Age1 = sc.nextInt();
+				System.out.println("How old are you working years?");
+				int YearsOfService = sc.nextInt();
+				System.out.println("Amount of your salary?");
+				int Salary = sc.nextInt();
+				System.out.println("Are you employed? Answer with: true or false.");
+				boolean Employed = sc.nextBoolean();
+				
+				if(Age1 >= 18 && YearsOfService >= 1 && Salary >= 300 && Employed == true) {
+					System.out.println("Your cash loan is ACCEPTED");
+				}else 
+					System.out.println("Your cash loan is NOT ACCEPTED");
+				
+				System.out.println("==========================================================================================================");
+				
+				System.out.println("Your village's data for cash loan is ---Name: " + clientName + "; Surname: " + clientSurName + "; Years: "  + Age1 + "; Years of service: " + YearsOfService + "; Salary: " + Salary + "; State of employment: " + Employed);
+			}
+		}
+	}
 }
 
